79ad14aa732a898e87b03e395af96881ff47c1e0
Claire Devlin <claire.devlin@hotmail.com>
2021-05-15 08:38:55 -0400

    first exercises

--- vampire.js
+++ vampire.js
@@ -10,22 +10,31 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let counter = 0;
+    let currentVamp = this;
 
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      counter++;
+    }
+    return counter;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    let output = this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
+    return output;
   }
 
   /** Stretch **/
@@ -36,6 +45,35 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let senior;
+    let junior
+    if (this.isMoreSeniorThan(vampire)) {
+      senior = this;
+      junior = vampire;
+    } else {
+      senior = vampire;
+      junior = this;
+    }
+
+    let distanceFromRoot1 = senior.numberOfVampiresFromOriginal;
+    let distanceFromRoot2 = junior.numberOfVampiresFromOriginal;
+
+    for (let i = 0; i < distanceFromRoot2 - distanceFromRoot1; i++) {
+      junior = junior.creator;
+    }
+    console.log("senior", senior);
+    console.log("junior", junior);
+
+    // let currentAncester = senior;
+    let commonAncester = "";
+    // check junior vamp's ancestor at the same seniority of senior vamp
+
+    //loop through older vamps ancestors
+    // while (!commonAncester) {
+    //   if (senior === junior) {
+
+    //   }
+    // }
 
   }
 }
